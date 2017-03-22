ba86837c4f5292ff6bee978d1816d4bc89fa5518
Edmund Law <etl29@scarletmail.rutgers.edu>
2017-03-22 01:17:50 -0400

    commit 1

---
+++ src/structures/IntervalTree.java
@@ -0,0 +1,202 @@
+package structures;
+
+import java.util.ArrayList;
+
+/**
+ * Encapsulates an interval tree.
+ * 
+ * @author runb-cs112
+ */
+public class IntervalTree {
+	
+	/**
+	 * The root of the interval tree
+	 */
+	IntervalTreeNode root;
+	
+	/**
+	 * Constructs entire interval tree from set of input intervals. Constructing the tree
+	 * means building the interval tree structure and mapping the intervals to the nodes.
+	 * 
+	 * @param intervals Array list of intervals for which the tree is constructed
+	 */
+	public IntervalTree(ArrayList<Interval> intervals) {
+		
+		// make a copy of intervals to use for right sorting
+		ArrayList<Interval> intervalsRight = new ArrayList<Interval>(intervals.size());
+		for (Interval iv : intervals) {
+			intervalsRight.add(iv);
+		}
+		
+		// rename input intervals for left sorting
+		ArrayList<Interval> intervalsLeft = intervals;
+		
+		// sort intervals on left and right end points
+		//System.out.println();
+		sortIntervals(intervalsLeft, 'l');
+		//System.out.println();
+		sortIntervals(intervalsRight,'r');
+		
+		// get sorted list of end points without duplicates
+		ArrayList<Integer> sortedEndPoints = 
+							getSortedEndPoints(intervalsLeft, intervalsRight);
+		
+		// build the tree nodes
+		root = buildTreeNodes(sortedEndPoints);
+		
+		// map intervals to the tree nodes
+		mapIntervalsToTree(intervalsLeft, intervalsRight);
+	}
+	
+	/**
+	 * Returns the root of this interval tree.
+	 * 
+	 * @return Root of interval tree.
+	 */
+	public IntervalTreeNode getRoot() {
+		return root;
+	}
+	
+	/**
+	 * Sorts a set of intervals in place, according to left or right endpoints.  
+	 * At the end of the method, the parameter array list is a sorted list. 
+	 * 
+	 * @param intervals Array list of intervals to be sorted.
+	 * @param lr If 'l', then sort is on left endpoints; if 'r', sort is on right endpoints
+	 */
+	public static void sortIntervals(ArrayList<Interval> intervals, char lr) {
+		// COMPLETE THIS METHOD
+		
+		int size = intervals.size();
+		for(int i = 0; i<size;i++){
+			for(int j = i+1; j<size;j++){
+				if(lr == 'l'){
+					int currLE = intervals.get(i).leftEndPoint;
+					int nextLE = intervals.get(j).leftEndPoint;		
+					if(nextLE<currLE){
+						Interval tmp = intervals.get(i);
+						intervals.set(i, intervals.get(j));
+						intervals.set(j, tmp);
+						
+					}
+				}
+				else{
+					int currRE = intervals.get(i).rightEndPoint;
+					int nextRE = intervals.get(j).rightEndPoint;		
+					if(nextRE<currRE){
+						Interval tmp = intervals.get(i);
+						intervals.set(i, intervals.get(j));
+						intervals.set(j, tmp);
+					}
+						
+					
+				}
+			}
+			
+		}
+		/*
+		for(int x = 0; x <size; x++){
+			System.out.println(intervals.get(x));
+		}
+		*/
+		
+	}
+	
+		
+	
+	/**
+	 * Given a set of intervals (left sorted and right sorted), extracts the left and right end points,
+	 * and returns a sorted list of the combined end points without duplicates.
+	 * 
+	 * @param leftSortedIntervals Array list of intervals sorted according to left endpoints
+	 * @param rightSortedIntervals Array list of intervals sorted according to right endpoints
+	 * @return Sorted array list of all endpoints without duplicates
+	 */
+	public static ArrayList<Integer> getSortedEndPoints(ArrayList<Interval> leftSortedIntervals, ArrayList<Interval> rightSortedIntervals) {
+		// COMPLETE THIS METHOD
+		
+		int size = leftSortedIntervals.size();
+		ArrayList <Integer> leftEnds = new ArrayList<Integer>(size);
+		ArrayList<Integer> rightEnds = new ArrayList<Integer>(size);
+		ArrayList<Integer> allSorted = new ArrayList<Integer>(2*size);
+		
+		
+		for(int i = 0; i < size; i ++){
+			int leftEnd = leftSortedIntervals.get(i).leftEndPoint;
+			
+			if(leftEnds.contains(leftEnd)){
+				continue;
+			}
+			else{
+				leftEnds.add(leftEnd);
+			}
+		}
+		for(int i = 0; i<size; i++){
+			int rightEnd = rightSortedIntervals.get(i).rightEndPoint;
+			if(rightEnds.contains(rightEnd)){
+				continue;
+			}
+			else{
+				rightEnds.add(rightEnd);
+			}
+		}
+		allSorted = leftEnds;
+		for(int i = 0; i<rightEnds.size();i++){
+			if(allSorted.contains(rightEnds.get(i)))
+				continue;
+			else
+				allSorted.add(rightEnds.get(i));
+				
+		}
+		for(int i = 0; i <allSorted.size(); i++){
+			for(int j = i+1; j<allSorted.size(); j++){
+				if(allSorted.get(i)>allSorted.get(j)){
+					int tmp = allSorted.get(i);
+					allSorted.set(i, allSorted.get(j));
+					allSorted.set(j, tmp);
+				}
+			}
+		}
+		System.out.println(allSorted);
+		return allSorted;
+		
+		
+	}
+	
+	/**
+	 * Builds the interval tree structure given a sorted array list of end points
+	 * without duplicates.
+	 * 
+	 * @param endPoints Sorted array list of end points
+	 * @return Root of the tree structure
+	 */
+	public static IntervalTreeNode buildTreeNodes(ArrayList<Integer> endPoints) {
+		// COMPLETE THIS METHOD
+		// THE FOLLOWING LINE HAS BEEN ADDED TO MAKE THE PROGRAM COMPILE
+		return null;
+	}
+	
+	/**
+	 * Maps a set of intervals to the nodes of this interval tree. 
+	 * 
+	 * @param leftSortedIntervals Array list of intervals sorted according to left endpoints
+	 * @param rightSortedIntervals Array list of intervals sorted according to right endpoints
+	 */
+	public void mapIntervalsToTree(ArrayList<Interval> leftSortedIntervals, ArrayList<Interval> rightSortedIntervals) {
+		// COMPLETE THIS METHOD
+	}
+	
+	/**
+	 * Gets all intervals in this interval tree that intersect with a given interval.
+	 * 
+	 * @param q The query interval for which intersections are to be found
+	 * @return Array list of all intersecting intervals; size is 0 if there are no intersections
+	 */
+	public ArrayList<Interval> findIntersectingIntervals(Interval q) {
+		// COMPLETE THIS METHOD
+		// THE FOLLOWING LINE HAS BEEN ADDED TO MAKE THE PROGRAM COMPILE
+		return null;
+	}
+
+}
+
